cb99d37c2faac8f71783fd1599cac39ae47a1dfb
wiky-avis <peta-holding@yandex.ru>
2021-05-13 12:00:14 +0300

    pep82

--- api/views.py
+++ api/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.filters import SearchFilter
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
 from .models import Follow, Group, Post, User
 from .permissions import IsOwnerOrReadOnly
